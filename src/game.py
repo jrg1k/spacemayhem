d8dc118c33e9c20533a3e01bb27fdcd27c7a6d56
Jørgen Kristensen <jk@jkristensen.net>
2021-03-17 04:23:53 +0100

    server is running

--- src/game.py
+++ src/game.py
@@ -1,59 +1,70 @@
 import pygame
-from pygame import sprite
 from pygame import Surface
 from pygame import Vector2
 from pygame.sprite import Sprite
 from pygame.sprite import Group
 
 
-class SpaceShip(Sprite):
-    """ its a spaceship """
-
-    def __init__(self, image=Surface, pos=(int, int), direction=(int, int),
-                 velocity=int):
-        super().__init__()
-        self.orig_image = image
-        self.velocity = Vector2(0, 0)
-        self.direction = Vector2(direction).normalize()
+class SpaceShip:
+    def __init__(self, pos, direction, velocity):
         self.pos = Vector2(pos)
-        self.update(0)
+        self.direction = Vector2(direction)
+        if self.direction.length_squared() == 0:
+            self.direction = Vector2(1, 0)
+        else:
+            self.direction.normalize_ip()
+        self.velocity = Vector2(velocity)
 
     def update(self, diff=float):
         length = self.velocity.length_squared()
         if length > 36:
             self.velocity.scale_to_length(6)
+        self.pos += self.velocity * diff
+
+    def data(self):
+        return ({
+            "pos": (self.pos.x, self.pos.y),
+            "dir": (self.direction.x, self.direction.y),
+            "velocity": (self.velocity.x, self.velocity.y)
+        })
+
+    def control(self, ctrl, diff=1):
+        if ctrl["left"]:
+            self.direction = self.direction.rotate(-5 * diff)
+        if ctrl["right"]:
+            self.direction = self.direction.rotate(5 * diff)
+        if ctrl["forward"]:
+            self.velocity += self.direction * diff * 0.2
+
+
+class LocalSpaceShip(Sprite):
+    """ its a spaceship """
+    def __init__(self, image=Surface):
+        super().__init__()
+        self.orig_image = image
+        self.update({"pos": (0, 0), "dir": (1, 0), "velocity": (0, 0)})
+
+    def update(self, data):
+        self.pos = Vector2(data["pos"])
+        print(self.pos)
+        self.direction = Vector2(data["dir"])
+        self.velocity = Vector2(data["velocity"])
         angle = self.direction.angle_to(Vector2(1, 0))
         self.image = pygame.transform.rotate(self.orig_image, angle)
         self.rect = self.image.get_rect()
-        self.pos += self.velocity * diff
         self.rect.center = self.pos
-        self.control(diff)
-
-    def control(self, diff):
-        print("Must be overridden")
 
 
-class Player(SpaceShip):
+class Player(LocalSpaceShip):
     """ player spaceship """
-
-    def __init__(self, pos, velocity):
+    def __init__(self):
         image = pygame.image.load("player.png")
         image = pygame.transform.scale(image, (20, 20))
-        super().__init__(image, pos, velocity)
-
-    def control(self, diff):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-            self.direction = self.direction.rotate(-5 * diff)
-        if key[pygame.K_RIGHT]:
-            self.direction = self.direction.rotate(5 * diff)
-        if key[pygame.K_UP]:
-            self.velocity += self.direction * diff * 0.2
+        super().__init__(image)
 
 
 class MayhemGame:
     """ The game """
-
     def __init__(self, SCREENW, SCREENH, FNAME_BG):
         screenwh = (SCREENW, SCREENH)
         self.screen = pygame.display.set_mode(screenwh)
@@ -63,12 +74,19 @@ class MayhemGame:
         else:
             self.background = pygame.image.load(FNAME_BG)
             self.background = pygame.transform.scale(self.background, screenwh)
-        self.player = Player((50, 50), (1, 0))
+        self.player = Player()
         self.ships = Group()
         self.ships.add(self.player)
+        self.control = {"left": 0, "right": 0, "forward": 0}
 
-    def update(self, diff):
-        self.ships.update(diff)
+    def update(self, data):
+        self.ships.update(data)
+        key = pygame.key.get_pressed()
+        self.control["left"] = key[pygame.K_LEFT]
+        self.control["right"] = key[pygame.K_RIGHT]
+        self.control["forward"] = key[pygame.K_UP]
+        print(self.control)
+        return self.control
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
--- src/smayhem.py
+++ src/smayhem.py
@@ -2,26 +2,68 @@
 
 import pygame
 import config
+import json
+import asyncio
 from game import MayhemGame
 
 
-def main():
+class Client:
+    def __init__(self, reader, writer):
+        self.latestupdate = {"pos": (0, 0), "dir": (1, 0), "velocity": (0, 0)}
+        self.data = {"left": 0, "right": 0, "forward": 0}
+        self.reader = reader
+        self.writer = writer
+
+
+async def send(client):
+    while True:
+        await asyncio.sleep(0.02)
+        msg = client.data
+        if msg is None:
+            continue
+        if client.writer.is_closing():
+            print("connection closed")
+            break
+        msg = json.dumps(msg) + "\n"
+        client.writer.write(msg.encode())
+        await client.writer.drain()
+
+
+async def recv(client):
+    print(1)
+    while True:
+        await asyncio.sleep(0.02)
+        data = await client.reader.readline()
+        if len(data) == 0 and client.reader.at_eof():
+            print("client disconnected")
+            break
+        msg = data.decode()
+        client.latestupdate = json.loads(msg)
+
+
+async def game(client):
     pygame.init()
     pygame.display.set_caption("Space Mayhem")
     game = MayhemGame(config.SCREENW, config.SCREENH, config.FNAME_BG)
 
-    clock = pygame.time.Clock()
-    diff = 0.0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print(event)
                 exit()
-        diff = clock.tick(60) / 20
+        await asyncio.sleep(0.02)
         game.draw()
-        game.update(diff)
+        data = game.update(client.latestupdate)
+        print(data)
+        client.data = data
         pygame.display.update()
 
 
+async def main():
+    reader, writer = await asyncio.open_connection("127.0.0.1", 55555)
+    client = Client(reader, writer)
+    await asyncio.gather(send(client), recv(client), game(client))
+
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
